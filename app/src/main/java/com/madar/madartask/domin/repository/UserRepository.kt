30e237245ac011ed44f82bf332af087bd3254173
ahmed_elbarody <ahmed.elbarody@adres.ae>
2026-02-25 00:17:42 +0200

    add user repository

---
+++ app/src/main/java/com/madar/madartask/domin/repository/UserRepository.kt
@@ -0,0 +1,22 @@
+package com.madar.madartask.domin.repository
+
+import com.madar.madartask.data.local.dao.UserDao
+import com.madar.madartask.domin.model.User
+
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+    suspend fun insertUser(user: User): Long
+
+    suspend fun updateUser(user: User)
+
+    suspend fun deleteUser(user: User)
+
+    //fun getAllUsers(): Flow<List<User>>
+
+    suspend fun getUserById(userId: Int): User?
+
+    suspend fun deleteAllUsers()
+
+    fun getUserCount(): Flow<Int>
+}
