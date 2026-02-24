bd24ae678740a1e9067a27004d71aa83ad3003cf
ahmed_elbarody <ahmed.elbarody@adres.ae>
2026-02-25 00:16:33 +0200

    add database

--- app/src/main/java/com/madar/madartask/data/local/dao/UserDao.kt
+++ app/src/main/java/com/madar/madartask/data/local/dao/UserDao.kt
@@ -1,1 +1,33 @@
 package com.madar.madartask.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.madar.madartask.data.local.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun insertUser(user: UserEntity): Long
+
+    @Update
+    suspend fun updateUser(user: UserEntity)
+
+    @Delete
+    suspend fun deleteUser(user: UserEntity)
+
+    @Query("SELECT * FROM users ORDER BY createdAt DESC")
+    fun getAllUsers(): Flow<List<UserEntity>>
+
+    @Query("SELECT * FROM users WHERE id = :userId")
+    suspend fun getUserById(userId: Int): UserEntity?
+
+    @Query("DELETE FROM users")
+    suspend fun deleteAllUsers()
+
+    @Query("SELECT COUNT(*) FROM users")
+    fun getUserCount(): Flow<Int>
+}
