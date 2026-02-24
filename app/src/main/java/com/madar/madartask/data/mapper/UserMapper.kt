a745c591955ee0f072d4c4c1f2d4861ed20abc53
ahmed_elbarody <ahmed.elbarody@adres.ae>
2026-02-25 00:17:13 +0200

    add user data class and userMapper

--- app/src/main/java/com/madar/madartask/data/mapper/UserMapper.kt
+++ app/src/main/java/com/madar/madartask/data/mapper/UserMapper.kt
@@ -1,2 +1,24 @@
 package com.madar.madartask.data.mapper
 
+import com.madar.madartask.data.local.entity.UserEntity
+import com.madar.madartask.domin.model.User
+
+fun UserEntity.toDomain(): User {
+    return User(
+        id = id,
+        name = name,
+        age = age,
+        jobTitle = jobTitle,
+        gender = gender
+    )
+}
+
+fun User.toEntity(): UserEntity {
+    return UserEntity(
+        id = id,
+        name = name,
+        age = age,
+        jobTitle = jobTitle,
+        gender = gender
+    )
+}
---
+++ app/src/main/java/com/madar/madartask/domin/model/User.kt
@@ -0,0 +1,9 @@
+package com.madar.madartask.domin.model
+
+data class User(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val jobTitle: String,
+    val gender: String
+)
